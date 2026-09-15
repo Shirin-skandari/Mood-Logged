e84693a5019a4978d6d74308bcd5fdfa61c52a51
Shirin <shirin.eskandari@outlook.com>
2026-09-15 23:08:27 +0330

    Add happy mood message

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const moodCards = document.querySelectorAll(".mood-card");
 const selectedMoodTitle = document.querySelector(".selected-mood h3");
+const selectedMoodMessage = document.querySelector(".selected-mood p");
 
 moodCards.forEach((moodCard) => {
     moodCard.addEventListener("click", () => {
         const moodName = moodCard.querySelector("p").textContent;
 
         selectedMoodTitle.textContent = `You chose: ${moodName}`;
+
+        if (moodName === "Happy") {
+            selectedMoodMessage.textContent = "Good things are coming, keep going";
+        }
     });
 });
